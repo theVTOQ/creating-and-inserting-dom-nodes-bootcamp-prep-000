1e040d2ae6182b17f53b5cb615417d380ce57328
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-18 00:05:59 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,6 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+var element = document.createElement('div');
+element.innerHTML = 'Hello, DOM!';
+element.style.background = '#f9f9f9';
+document.body.appendChild(element);
